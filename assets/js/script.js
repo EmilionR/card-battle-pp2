89543c5a0a5242aaa77b9bc2e04a81cdb614eba2
Emilion <emiljadersten@gmail.com>
2023-12-12 18:29:04 +0100

    Add first functions to javascript

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+
+let handSize = 4;
+let playerScore = 0;
+let enemyScore = 0;
+
+/**
+ * Initialize game state,
+ * create base classes & reset relevant variables
+ */
+function initGameState() {
+
+}
+
+/**
+ * Draw card into the selected hand
+ */
+function drawCard() {
+
+}
