31d5ed9f74a22c7721264f51b43ffbd248553b94
Emilion <emiljadersten@gmail.com>
2023-12-13 20:53:05 +0100

    Add function to close help menu

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -5,3 +5,7 @@ function openHelp(){
     //Make "how to play" visible
     document.getElementById("info-screen").style.display = "block";
 }
+
+function closeMenu() {
+    document.getElementById("info-screen").style.display = "none";
+}
